4459ceeaa95d8f139eed2abd5297dcd012212b5a
Luka Kuterovac <luka.kuterovac@gmail.com>
2023-10-31 17:14:16 +0100

    Add project

---
+++ RUAP-LV/RUAP-LV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_LV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Luke Kuterovca");
+        }
+    }
+}
